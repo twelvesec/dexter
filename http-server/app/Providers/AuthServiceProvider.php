cb49311873373143e2c5525e709d3a810efe4bea
maldevel <maldevel@mail.com>
2018-09-24 16:17:29 +0200

    configuring passport

--- http-server/app/Providers/AuthServiceProvider.php
+++ http-server/app/Providers/AuthServiceProvider.php
@@ -27,5 +27,9 @@ class AuthServiceProvider extends ServiceProvider
         $this->registerPolicies();
 
         Passport::routes();
+
+        Passport::tokensExpireIn(now()->addDays(15));
+
+        Passport::refreshTokensExpireIn(now()->addDays(30));
     }
 }
