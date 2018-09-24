daad87f7db1bbc04d5fc414858e7185470dd0fb0
maldevel <maldevel@mail.com>
2018-09-24 12:22:08 +0000

    building api

--- http-server-mysql-db.sql
+++ http-server-mysql-db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE dexter;
 
 /* change the default password here and also in Laravel database config file */
-CREATE USER 'dexteruser'@'localhost' IDENTIFIED BY '013579abcd';
+CREATE USER 'dexteruser'@'localhost' IDENTIFIED BY '0123456789abcd';
 
 GRANT ALL PRIVILEGES ON dexter.* TO 'dexteruser'@'localhost';
 
