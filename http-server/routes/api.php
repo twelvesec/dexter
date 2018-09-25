6556671e074482228fd60483def4dad4eb75d3e9
maldevel <maldevel@mail.com>
2018-09-25 12:16:22 +0200

    api user auth

--- http-server/routes/api.php
+++ http-server/routes/api.php
@@ -13,9 +13,9 @@ use Illuminate\Http\Request;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
-});
+});*/
 
 Route::middleware('auth:api')->group( function () {
     Route::resource('computers', 'API\ComputerController');
