cf6c745db841ee2d497c805beca146fbc59c52fa
maldevel <maldevel@mail.com>
2018-11-10 23:07:29 +0200

    minor

--- http-server/app/Providers/AuthServiceProvider.php
+++ http-server/app/Providers/AuthServiceProvider.php
@@ -28,8 +28,8 @@ class AuthServiceProvider extends ServiceProvider
 
         Passport::routes();
 
-        Passport::tokensExpireIn(now()->addDays(5));
+        Passport::tokensExpireIn(now()->addMinutes(30));
 
-        Passport::refreshTokensExpireIn(now()->addDays(10));
+        Passport::refreshTokensExpireIn(now()->addHours(12));
     }
 }
